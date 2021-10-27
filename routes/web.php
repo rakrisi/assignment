942d1b964c49868d3447a667f7f19a6f4a194f87
rakrisi <radhesingh12321@gmail.com>
2021-10-28 04:00:28 +0530

    model controller created

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/company', CompanyController::class);
+Route::resource('/employee', EmployeeController::class);
+
